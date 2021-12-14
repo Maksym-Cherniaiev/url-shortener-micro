24d06a3a1ed52cd1090101b3b11edd47500f4b54
Max <maksym.cherniaiev@ralabs.org>
2021-12-14 17:29:39 +0200

    [FIX] process env variables

--- database/connect-params.ts
+++ database/connect-params.ts
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+
 export const connectParams = {
 	host: process.env.HOST || 'localhost',
-	user: process.env.USER || 'likeran',
+	user: process.env.USER || 'postgres',
 	port: 5432,
 	password: process.env.PASS || 'password',
 	database: process.env.DB || 'postgres'
